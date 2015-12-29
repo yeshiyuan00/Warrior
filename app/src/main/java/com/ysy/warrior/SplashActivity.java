7544cadde4e4f2e82944b1731e173954fa273372
yeshiyuan <yeshiyuan00@gmail.com>
2015-12-29 15:16:25 +0800

    project init

---
+++ app/src/main/java/com/ysy/warrior/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.ysy.warrior;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
