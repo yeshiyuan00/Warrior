ee06e613e6031723aab34e19d9fa7fe4e1444d85
yeshiyuan <yeshiyuan00@gmail.com>
2015-12-30 17:06:10 +0800

    material menu(todo)

--- app/src/main/java/com/ysy/warrior/activity/MainActivity.java
+++ app/src/main/java/com/ysy/warrior/activity/MainActivity.java
@@ -1,54 +1,35 @@
 package com.ysy.warrior.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.ysy.warrior.R;
+import com.ysy.warrior.view.meterialmenu.MaterialMenuView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+    private static final int ANIM_DURATION_TOOLBAR = 300;
+    private DrawerLayout drawerLayout;
+    private MaterialMenuView materialMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void initTitleBar(ViewGroup rl_title, TextView tv_title, ImageButton ib_back, ImageButton ib_right, View shadow) {
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected View getContentView() {
+        return null;
     }
+
 }
---
+++ app/src/main/java/com/ysy/warrior/view/NNN.java
@@ -0,0 +1,9 @@
+package com.ysy.warrior.view;
+
+/**
+ * User: ysy
+ * Date: 2015/12/30
+ * Time: 16:52
+ */
+public class NNN {
+}
